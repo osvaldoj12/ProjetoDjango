12ee83c1d07a1fc0d91719d10c7ddc9f8e6ab239
Osvaldo <osvaldojuniorlima@hotmail.com>
2019-12-07 11:50:37 -0300

    Erro no Formulario

---
+++ Erro formulario/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+
+from .forms import SugestoesForm
+
+
+def get_name(request):
+    if request.method == 'POST':
+        form = SugestoesForm(request.POST)
+        if form.is_valid():
+            SugestoesForm = form.save(commit=False)
+            SugestoesForm.save()
+            return HttpResponseRedirect('/ouvidoria/')
+
+    else:
+        form = SugestoesForm()
+
+    return render(request, 'Sugestao.html', {'form': form})
